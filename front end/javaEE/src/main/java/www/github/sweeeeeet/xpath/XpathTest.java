78934b73a065bcee9ac67df37670c627a2921f3f
TangLing <1158354636@qq.com>
2019-04-25 18:55:32 +0800

    Xpath basic knowledge

---
+++ front end/javaEE/src/main/java/www/github/sweeeeeet/xpath/XpathTest.java
@@ -0,0 +1,20 @@
+package www.github.sweeeeeet.xpath;
+
+/**
+ * Author:sweet
+ * Created:2019/4/20
+ */
+public class XpathTest {
+/*
+* 模拟用户登陆：输入用户名和密码，如果用户名不村子啊，提示用户名不存在，登陆失败
+*       如果输入密码不一致，提示密码错误，登陆失败
+*  xml可以作为一个小型数据库
+*  （1）设计user.xml
+*  （2）键盘录入数据（Scanner/流BufferedReader）{
+*  System.in是字节流；
+*  字符转换流：new InputStreamReader(System.in):特有的功能，一次读取一行：readLine()}
+*  (3)使用解析器对象读取文件
+*  使用xpath定位：不分层级关系下的name标签的文本，如果节点不存在就提示登陆失败
+*                                           如果存在 判断密码
+* */
+}
