b4c2fab478dbc0976921db2cf7c5080d0ef966b9
TangLing <1158354636@qq.com>
2019-06-04 14:54:57 +0800

    sax parasing

---
+++ front end/xml-parse/src/main/java/com/github/sweeeeeet/sax/MySAX.java
@@ -0,0 +1,28 @@
+package com.github.sweeeeeet.sax;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+/**
+ * Author:sweet
+ * Created:2019/4/20
+ */
+public class MySAX {
+    /*SAX解析
+    * 开发步骤：创建SAX解析器对象
+    * 读取contact.xml文件
+    * 注册事件监听器：DefaultHandler,扩展此类
+    * 自定义一个类，继续    DefaultHandler，覆盖它里面的方法
+
+     * */
+    public static void main(String[] args) throws Exception {
+        //创建SAXParser解析器对象,SAXParser是一个抽象类，他的实例需要通过工厂方法实现
+        SAXParser parser=SAXParserFactory.newInstance().newSAXParser();//多态
+        //解析
+        //注册监听事件
+        MyDefaultHandler1 handler2=new MyDefaultHandler1();
+        parser.parse(Thread.currentThread().getContextClassLoader().getResourceAsStream("contact.xml"),handler2);
+        String content=handler2.getString();
+        System.out.println(content);
+    }
+}
