4e6196675394eb03991599ddbce7e4f91c048e96
TangLing <1158354636@qq.com>
2019-06-06 22:45:32 +0800

    basic knowledge of mybits

---
+++ front end/javaEE/mybits/src/main/java/com/github/sweeeeeet/mybatis/mapper/MemoGroupMapper.java
@@ -0,0 +1,11 @@
+package com.github.sweeeeeet.mybatis.mapper;
+
+import com.github.sweeeeeet.mybatis.entity.MemoGroup;
+
+/**
+ * Author:sweet
+ * Created:2019/5/10
+ */
+public interface MemoGroupMapper {
+    int insertMemoGroup(MemoGroup memoGroup);
+}
