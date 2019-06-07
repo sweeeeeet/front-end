667c1fe6fead51b024fbcd051365d9475938a127
TangLing <1158354636@qq.com>
2019-06-07 08:31:21 +0800

    basic knowledge of mybatis

---
+++ front end/javaEE/mybits/src/main/java/com/github/sweeeeeet/mybatis/mapper/SqlSessionUsage.java
@@ -0,0 +1,13 @@
+package com.github.sweeeeeet.mybatis.mapper;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+
+/**
+ * Author:sweet
+ * Created:2019/5/10
+ */
+public class SqlSessionUsage {
+    public static void main(String[] args) {
+        SqlSessionFactory sqlSessionFactory=new StringBuilder()
+    }
+}
