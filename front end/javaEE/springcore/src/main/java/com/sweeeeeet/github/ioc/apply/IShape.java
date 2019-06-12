c067c625620d3df5787a7ef927dee0a720a6916b
TangLing <1158354636@qq.com>
2019-06-12 23:27:37 +0800

    basic knowledge of ioc

---
+++ front end/javaEE/springcore/src/main/java/com/sweeeeeet/github/ioc/apply/IShape.java
@@ -0,0 +1,17 @@
+package com.sweeeeeet.github.ioc.apply;
+
+/**
+ * Author:sweet
+ * Created:2019/5/19
+ */
+public interface IShape {
+    /*
+    * 计算图形的面积
+    * */
+    double computeArea();
+    /*
+    * 计算图形的边长
+    * */
+    double computeSide();
+
+}
