2b0c269e315d52a10f13a0fad50ce0e96c818618
TangLing <1158354636@qq.com>
2019-06-14 11:43:27 +0800

    springframe----vo tier

---
+++ front end/javaEE/spring-frame/src/main/java/com/sweet/vo/UserInfo.java
@@ -0,0 +1,35 @@
+package com.sweet.vo;
+
+/**
+ * Author:sweet
+ * Created:2019/5/18
+ */
+public class UserInfo {
+    private String userName;
+    private String password;
+    private int userAge;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getUserAge() {
+        return userAge;
+    }
+
+    public void setUserAge(int userAge) {
+        this.userAge = userAge;
+    }
+}
