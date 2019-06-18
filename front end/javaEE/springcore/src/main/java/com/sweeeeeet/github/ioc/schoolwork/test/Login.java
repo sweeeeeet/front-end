e1bff71324e9c11a31f87627de3ef143986467ce
TangLing <1158354636@qq.com>
2019-06-18 14:37:31 +0800

    schoolwork---login

---
+++ front end/javaEE/springcore/src/main/java/com/sweeeeeet/github/ioc/schoolwork/test/Login.java
@@ -0,0 +1,13 @@
+package com.sweeeeeet.github.ioc.schoolwork.test;
+
+/**
+ * Author:sweet
+ * Created:2019/6/6
+ */
+public class Login
+{
+    public static void main(String args[])
+    {
+        WyFrame tl=new WyFrame();
+    }
+}
