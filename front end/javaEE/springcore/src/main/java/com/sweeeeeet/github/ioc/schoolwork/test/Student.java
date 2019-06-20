8a526fab72cb3e3cfe3f336b405dad6f04c0938a
TangLing <1158354636@qq.com>
2019-06-20 22:22:17 +0800

    schoolwork---student

---
+++ front end/javaEE/springcore/src/main/java/com/sweeeeeet/github/ioc/schoolwork/test/Student.java
@@ -0,0 +1,54 @@
+package com.sweeeeeet.github.ioc.schoolwork.test;
+
+/**
+ * Author:sweet
+ * Created:2019/6/6
+ */
+public class Student {
+    public char sex;
+    public String name;
+    public String birthday;
+    public String number;
+    public int compScore;
+    public int engScore;
+    public int mathScore;
+    public int averScore;
+    public int MinScore;
+    public int MaxScore;
+
+
+    public void setcompScore(int i) {
+
+    }
+
+    public void setengScore(int i) {
+
+    }
+
+    public char[] getcompScore() {
+return null;
+    }
+
+    public void setmathScore(int i) {
+    }
+
+    public char[] getengScore() {
+        return null;
+    }
+
+    public char[] getmathScore() {
+        return null;
+    }
+
+    public char[] getMaxScore() {
+        return null;
+    }
+
+    public char[] getMinScore() {
+        return null;
+    }
+
+    public char[] getAverScore() {
+        return null;
+    }
+}
