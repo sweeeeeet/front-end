cb67baf7f3fe906e5311aca52922512a4175770e
TangLing <1158354636@qq.com>
2019-11-22 17:05:49 +0800

    mybatis

---
+++ mybatis-expend/src/main/java/com/github/sweeeeeet/bz/UserBZ.java
@@ -0,0 +1,19 @@
+package com.github.sweeeeeet.bz;
+
+import com.github.sweeeeeet.po.UserExpend;
+
+/**
+ * Author:sweet
+ * Created:2019/11/21
+ */
+public class UserBZ {
+    private UserExpend userExpend;
+
+    public UserExpend getUserExpend() {
+        return userExpend;
+    }
+
+    public void setUserExpend(UserExpend userExpend) {
+        this.userExpend = userExpend;
+    }
+}
